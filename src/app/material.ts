268f580bddf26cbf3fa78ec656ff2e294f0e4424
tavisca-dpingale <dpingale@tavisca.com>
2018-12-04 12:13:52 +0530

    removed flex layout import

--- src/app/material.ts
+++ src/app/material.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
-    imports: [MatButtonModule, MatCheckboxModule,MatToolbarModule,MatIconModule,MatInputModule],
-    exports: [MatButtonModule, MatCheckboxModule,MatToolbarModule,MatIconModule,MatInputModule],
+    imports: [MatButtonModule, MatCheckboxModule,MatToolbarModule,MatIconModule,MatInputModule,MatGridListModule,MatCardModule],
+    exports: [MatButtonModule, MatCheckboxModule,MatToolbarModule,MatIconModule,MatInputModule,MatGridListModule,MatCardModule],
 })
 
 export class MaterialModule{}
--- src/app/toolbarlayout/toolbarlayout.component.ts
+++ src/app/toolbarlayout/toolbarlayout.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
 @Component({
   selector: 'app-toolbarlayout',
   templateUrl: './toolbarlayout.component.html',
   styleUrls: ['./toolbarlayout.component.scss']
 })
+
 export class ToolbarlayoutComponent implements OnInit {
+
+  tiles: Tile[] = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
 
   constructor() { }
 
@@ -13,3 +28,5 @@ export class ToolbarlayoutComponent implements OnInit {
   }
 
 }
+
+
